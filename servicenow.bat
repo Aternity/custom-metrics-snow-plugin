c91f52d5db0cf86296e3725a75fff0afd68babeb
Levi Bracken <levi.bracken@riverbed.com>
2020-06-24 14:31:08 -0400

    Initial drop of Custom Metrics SNOW Plugin

---
+++ servicenow.bat
@@ -0,0 +1,9 @@
+@echo off
+rem -----------------------------------------------------------------------------
+rem
+rem Description:  Script to start Service Now java cmx generator
+rem 
+rem 06/09/2020   com   initial version
+rem -----------------------------------------------------------------------------
+
+../jre/bin/java -jar service-now-generator-1.0-jar-with-dependencies.jar
